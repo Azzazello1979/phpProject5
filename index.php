10ad7e354b6ff60650ff4a585cee59a394914d29
Balint H <balint.haui@gmail.com>
2020-03-15 14:45:35 +0100

    bringing in jQuery

--- index.php
+++ index.php
@@ -12,17 +12,22 @@
 <h2 class="my-4 text-center">React Course ($25)</h2>
     <form action="charge.php" method="POST" id="payment-form">
         <div class="form-row">
+            <input type="text" class="form-control mb-3 StripeElement StripeElement--empty" name="first_name" placeholder="first name">
+            <input type="text" class="form-control mb-3 StripeElement StripeElement--empty" name="last_name" placeholder="last name">
+            <input type="email" class="form-control mb-3 StripeElement StripeElement--empty" name="email" placeholder="email address">
+            
             <div id="card-element" class="form-control">
-                <!-- Stripe element will go here -->
+                <!-- Stripe element will go in here -->
             </div>
-
-            <div id="form-errors">
-                <!-- Display card errors -->
+                
+            <div id="form-errors" role="alert">
+                <!-- Display form errors in here -->
             </div>
         </div>
         <button type="submit" class="btn btn-primary btn-block mt-3">Submit Payment</button>
     </form>
 </div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://js.stripe.com/v3/"></script>
 <script src="js/charge.js"></script>
 </body>
